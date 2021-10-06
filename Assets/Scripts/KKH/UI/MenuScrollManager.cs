a034f0e9aa71da8dd12ae5beb2365b5240e6a6bb
nexpang <rlarudgur123@gmail.com>
2021-10-06 15:42:33 +0900

    일단 틀 만듬

--- Assets/Scripts/KKH/UI/MenuScrollManager.cs
+++ Assets/Scripts/KKH/UI/MenuScrollManager.cs
@@ -2,33 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class MenuScrollManager : MonoBehaviour
+public class MenuScrollManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
+    RectTransform rectTrans;
     public Transform contentsParent;
-    private float contentsWidth;
-    private float contentsCount;
-    private float padding;
-    private float cameraWidth;
-    private float cameraHeight;
+    public float contentsWidth = 1400f;
+    private int contentsCount;
+
+    private float[] contentCenters;
+
+    private void Awake()
+    {
+        rectTrans = contentsParent.GetComponent<RectTransform>();
+    }
 
     void Start()
     {
-        cameraHeight = Camera.main.orthographicSize * 2;
-        cameraWidth = Camera.main.aspect * cameraHeight;
-        contentsWidth = contentsParent.GetChild(0).GetComponent<RectTransform>().rect.width;
-        contentsCount = contentsParent.GetComponent<RectTransform>().rect.width / contentsWidth;
-        padding = (contentsWidth - cameraWidth)/2;
+        //contentsWidth = contentsParent.GetChild(0).GetComponent<RectTransform>().rect.width;
+        contentsCount = (int)(contentsParent.GetComponent<RectTransform>().rect.width / contentsWidth);
+        contentCenters = new float[contentsCount];
+        contentCenters[0] = 0;
+        for (int i = 1; i < ((contentsCount-1)/2)+1; i++)
+        {
+            contentCenters[i * 2 - 1] = (contentsWidth * i);
+            contentCenters[i * 2] = -(contentsWidth * i);
+        }
+
+        for (int i = 0; i < contentCenters.Length; i++)
+        {
+            print(contentCenters[i]);
+        }
     }
 
     public void SetTransfromScroll()
     {
-
+        RectTransform rectTrans = contentsParent.GetComponent<RectTransform>();
+        int idx = 0;
+        float min = Mathf.Abs(contentCenters[0] - rectTrans.anchoredPosition.x);
+        for (int i = 1; i < contentCenters.Length; i++)
+        {
+            float dist = Mathf.Abs(contentCenters[i] - rectTrans.anchoredPosition.x);
+            if (min > dist)
+            {
+                min = dist;
+                idx = i;
+            }
+        }
+        DOTween.To(() => rectTrans.anchoredPosition, x => rectTrans.anchoredPosition = x, new Vector2(contentCenters[idx], 0), 0.5f);
+        //rectTrans.DOAnchorPosX(contentCenters[idx], 1f);
+        //rectTrans.anchoredPosition = new Vector2(contentCenters[idx], rectTrans.anchoredPosition.y);
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Start");
+        DOTween.KillAll();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEndDrag(PointerEventData eventData)
     {
-        
+        //Debug.Log("EndDrag");
+        SetTransfromScroll();
     }
 }
