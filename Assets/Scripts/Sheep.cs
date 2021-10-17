450f62091140b32ed848127543c22ec43146822d
승핵 <sping__@naver.com>
2021-10-18 08:25:55 +0900

    대충이상한 양움직임 심심해서 만들어봄

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -1,5 +1,5 @@
+using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sheep : MonoBehaviour
@@ -7,22 +7,38 @@ public class Sheep : MonoBehaviour
     private PlayerInput playerInput = null;
     private JumpForce jumpForceCS;
     private Rigidbody2D rb = null;
+    private float moveForwardDist = 5f;
+    private float moveForwardTime = 3f;
 
     [Header("점프 관련")]
     [SerializeField] private float jumpForce = 13f;
     [SerializeField] private float fallMult = 2.5f;
     [SerializeField] private float lowJumpMult = 5f;
 
+    Sequence sq;
+
     private void Start()
     {
         playerInput = FindObjectOfType<PlayerInput>();
         jumpForceCS = FindObjectOfType<JumpForce>();
         rb = GetComponent<Rigidbody2D>();
+
+        sq = DOTween.Sequence()
+            .SetAutoKill(false)
+            .Append(transform.DOMoveX(transform.position.x + moveForwardDist, 1f))
+            .AppendInterval(moveForwardTime)
+            .Append(transform.DOMoveX(transform.position.x, 1f))
+            .Pause();
     }
 
     private void Update()
     {
         Jump();
+    }
+
+    private void MoveForward()
+    {
+        sq.Restart();
     }
 
     private void Jump()
@@ -31,7 +47,15 @@ public class Sheep : MonoBehaviour
         if (playerInput.Jump_Up)
         {
             rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * jumpForce * jumpForceCS.jumpForce01Temp, ForceMode2D.Impulse);
+            if (jumpForceCS.jumpForce01Temp < 0.2f)
+            {
+                MoveForward();
+            }
+            else
+            {
+                rb.AddForce(Vector2.up * jumpForce * jumpForceCS.jumpForce01Temp, ForceMode2D.Impulse);
+
+            }
         }
 
         // 아직 붕 뜨고 있는 상태
