f47633967d76f34520cd00f87abf0dbc8eb8b66d
nexpang <rlarudgur123@gmail.com>
2021-12-04 02:43:16 +0900

    Revert "Revert "룰렛 착용 완성인데 코드를 좀 다듬을 필요가 있어요""
    This reverts commit d100e9ed2fa6b99e124d55dae02d1732e79518da.

--- Assets/Scripts/ROL/Rullet/SkillRullet.cs
+++ Assets/Scripts/ROL/Rullet/SkillRullet.cs
@@ -6,19 +6,109 @@ using DG.Tweening;
 
 public class SkillRullet : Rullet
 {
+    public GameObject nomalRulletPrefab;
     public Image borderImg;
+    private bool isRollingReward = false;
+
+    protected override void Start()
+    {
+        GetComponentsInChildren(pieces);
+
+        SetRullet();
+        int sizeSum = 0;
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            sizeSum += pieces[i].Size;
+        }
+        int addNomalCnt = (maxSize - sizeSum) / 6;
+        AddNormalAttackPiece(addNomalCnt);
+        RollRullet();
+    }
+
+    public void StopRulletToChangePiece()
+    {
+        isRollingReward = true;
+        StopRullet();
+    }
+
+    public void ChangePiece(int changeIdx, RulletPiece changePiece)
+    {
+        Destroy(pieces[changeIdx].gameObject);
+        pieces[changeIdx] = changePiece;
+        SetRulletSmooth();
+    }
+
+
+    public void AddNormalAttackPiece(int addCnt)
+    {
+        for (int i = 0; i < addCnt; i++)
+        {
+            GameObject nomalRullet = Instantiate(nomalRulletPrefab, Vector3.zero, Quaternion.identity, gameObject.transform);
+            pieces.Add(nomalRullet.GetComponent<RulletPiece>());
+            nomalRullet.transform.localPosition = Vector3.zero;
+        }
+        SetRullet();
+    }
 
     protected override void RulletResult()
     {
-        base.RulletResult();
+        // 총 각도의 합
+        float angleSum = 0f;
+        // 크기 1당 각도
+        float sizeAngle = 360f / maxSize;
 
-        if (result != null)
+        // -180 ~ 180 표현식을 0 ~ 360 표현식으로 변환하는 과정
+        float rulletAngle = Quaternion.FromToRotation(Vector3.right, transform.right).eulerAngles.z;
+        //print("룰렛 각도 : " + rulletAngle);
+
+        int pieceIdx = -1;
+
+        for (int i = 0; i < pieces.Count; i++)
         {
-            borderImg.DOColor(result.Color, 0.55f);
-            borderImg.GetComponent<RotateBorder>().SetSpeed(true);
+            // 범위 안에 있다면
+            if (rulletAngle <= angleSum + pieces[i].Size * sizeAngle
+                && rulletAngle >= angleSum)
+            {
+                pieceIdx = i;
+                break;
+            }
+
+            // 시작 범위를 해당 조각의 사이즈만큼 증가
+            angleSum += pieces[i].Size * sizeAngle;
         }
 
-        GameManager.Instance.battleHandler.results.Add(result);
+
+        if (pieceIdx >= 0)
+        {
+            print($"{pieceIdx + 1}번째 당첨!");
+            result = pieces[pieceIdx];
+        }
+        else
+        {
+            //print("기본 당첨!");
+            CastDefault();
+        }
+
+        if (!isRollingReward)
+        {
+            fillTween = result.GetComponent<Image>().DOFillAmount(1f, 0.6f);
+            result.transform.SetAsLastSibling();
+            result.transform.DOScale(new Vector3(1.1f, 1.1f, 1f), 0.55f);
+            result.Highlight();
+
+            if (result != null)
+            {
+                borderImg.DOColor(result.Color, 0.55f);
+                borderImg.GetComponent<RotateBorder>().SetSpeed(true);
+            }
+
+            GameManager.Instance.battleHandler.results.Add(result);
+        }
+        else
+        {
+            GameManager.Instance.inventoryHandler.result = result;
+            GameManager.Instance.inventoryHandler.resultIdx = pieceIdx;
+        }
     }
 
     protected override void CastDefault()
