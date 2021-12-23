75672e3517ad1408b9336e6b7d948588531552a7
nexpang <rlarudgur123@gmail.com>
2021-12-23 16:58:38 +0900

    플레이어가 공격하는 애니메이션 대충 만들어놈 SkillPiece에 넣어놈 적용할꺼면 base.Cast()ㄱㄱ

--- Assets/Scripts/Piece/SkillPieces/base/SkillPiece.cs
+++ Assets/Scripts/Piece/SkillPieces/base/SkillPiece.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SkillPiece : RulletPiece
 {
@@ -8,6 +9,16 @@ public class SkillPiece : RulletPiece
 
     public override void Cast()
     {
+        PlayerAttackAnimation();
+    }
 
+    private void PlayerAttackAnimation()
+    {
+        Transform playerTrm = GameManager.Instance.battleHandler.player.transform;
+        Vector3 enermyPos = GameManager.Instance.battleHandler.enemy.transform.position;
+        Vector3 playerPos = playerTrm.position;
+
+
+        Sequence sequence = DOTween.Sequence().Append(playerTrm.DOMoveX(Mathf.Lerp(playerPos.x, enermyPos.x, 0.3f), 0.1f)).Append(playerTrm.DOMoveX(playerPos.x, 0.2f));
     }
 }
