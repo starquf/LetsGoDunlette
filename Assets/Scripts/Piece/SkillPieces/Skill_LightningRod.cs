767bde6b0c7046ba0e1dd79a32ea62362154cc0f
nexpang <rlarudgur123@gmail.com>
2021-12-24 01:46:38 +0900

    피뢰침 개사기네요

--- Assets/Scripts/Piece/SkillPieces/Skill_LightningRod.cs
+++ Assets/Scripts/Piece/SkillPieces/Skill_LightningRod.cs
@@ -12,14 +12,38 @@ public class Skill_LightningRod : SkillPiece
         base.Cast();
         print($"스킬 발동!! 이름 : {PieceName}");
 
-        Vector3 target = GameManager.Instance.battleHandler.enemy.transform.position;
+        BattleHandler battleHandler = GameManager.Instance.battleHandler;
+
+        Rullet rullet = battleHandler.rullets[0];
+        List<RulletPiece> skillPieces = rullet.GetPieces();
+        List<SkillPiece> lightningSkillPieces = new List<SkillPiece>();
+        Dictionary<SkillPiece, int> lightningSkillIdxDic = new Dictionary<SkillPiece, int>();
+        for (int i = 0; i < skillPieces.Count; i++)
+        {
+            if(skillPieces[i].PieceType.Equals(PieceType.SKILL))
+            {
+                if(skillPieces[i].comboType.Equals(EComboType.Diamonds) && skillPieces[i] != this)
+                {
+                    SkillPiece piece = skillPieces[i] as SkillPiece;
+                    lightningSkillPieces.Add(piece);
+                    lightningSkillIdxDic.Add(piece, i);
+                }
+            }
+        }
+
+        Vector3 target = battleHandler.enemy.transform.position;
         target.y -= 0.7f;
         target.x += 0.5f;
 
         Effect_LightningRod lightningRodEffect = Instantiate(LightningRodEffectPrefab, target, Quaternion.identity).GetComponent<Effect_LightningRod>();
 
         lightningRodEffect.Play("LightningRodEffect", () => {
-
+            if(lightningSkillPieces.Count > 0)
+            {
+                SkillPiece skillPiece = lightningSkillPieces[Random.Range(0, lightningSkillPieces.Count)];
+                skillPiece.Cast();
+                battleHandler.ChangeRulletPiece(lightningSkillIdxDic[skillPiece]);
+            }
         });
 
         GameManager.Instance.battleHandler.enemy.GetDamage(Value);
