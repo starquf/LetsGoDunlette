4c693f8582fbc31506c340f8d18f4c3f643d732b
승핵 <sping__@naver.com>
2021-12-31 07:59:48 +0900

    폐기된 룰렛

---
+++ Assets/Scripts/SlotMachine/SlotMachine.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMachine : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] private Reel[] reels;
+}
