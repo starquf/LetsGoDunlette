44c7853ff0e36d85f973a0c8b6926a5490421a56
승핵 <sping__@naver.com>
2022-01-17 18:37:19 +0900

    미믹 제작 ,스킬 상처 제작
    상처 가 안되는거같긴한데 확인 부탁함

---
+++ Assets/Scripts/Piece/SkillPieces/Enemy/mimic/MI_Bump.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MI_Bump : SkillPiece
+{
+    public GameObject attackExpPrefab;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        isPlayerSkill = false;
+    }
+
+    public override void Cast(Action onCastEnd = null)
+    {
+        print($"적 스킬 발동!! 이름 : {PieceName}");
+        GameManager.Instance.cameraHandler.ShakeCamera(0.5f, 0.15f);
+
+        Vector3 target = GameManager.Instance.battleHandler.player.transform.position;
+        Vector3 startPos = skillImg.transform.position;
+
+        for (int i = 0; i < 3; i++)
+        {
+            EffectObj attackObj = PoolManager.GetItem<EffectObj>();
+            attackObj.transform.position = startPos;
+
+            int a = i;
+
+            attackObj.Play(target, () =>
+            {
+                attackObj.Sr.DOFade(0f, 0.1f)
+                    .OnComplete(() =>
+                    {
+                        attackObj.EndEffect();
+                    });
+
+                Instantiate(attackExpPrefab, attackObj.transform.position, Quaternion.identity);
+                GameManager.Instance.cameraHandler.ShakeCamera(0.25f, 0.2f);
+
+                if (a == 2)
+                    onCastEnd?.Invoke();
+            }
+            , BezierType.Cubic, i * 0.1f);
+
+        }
+
+        GameManager.Instance.battleHandler.player.GetDamage(Value);
+    }
+}
