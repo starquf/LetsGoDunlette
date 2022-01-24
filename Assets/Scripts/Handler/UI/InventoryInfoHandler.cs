c541c2ade911784a273a05e869b863c63d365fa2
starquf <starquf321@gmail.com>
2022-01-24 14:31:20 +0900

    페어리 버그 고침 최종 + 인벤토리 UI 효과 추가 + 조각 선택 UI 카드 크기 키움

--- Assets/Scripts/Handler/UI/InventoryInfoHandler.cs
+++ Assets/Scripts/Handler/UI/InventoryInfoHandler.cs
@@ -10,7 +10,14 @@ public class InventoryInfoHandler : MonoBehaviour
     private RectTransform rect;
     private RectTransform contentRect;
 
-    private Vector3 startPos;
+    private float startPos;
+    private float endPos;
+
+    [Header("움직이는 것들")]
+    public Transform bottomBG;
+
+    private float bgStartPos;
+    private float bgEndPos;
 
     [Header("버튼")]
     public Button invenBtn;
@@ -36,25 +43,31 @@ public class InventoryInfoHandler : MonoBehaviour
     {
         cg = GetComponent<CanvasGroup>();
         rect = GetComponent<RectTransform>();
-        startPos = cg.transform.localPosition;
+
+        startPos = cg.transform.localPosition.y;
+        endPos = cg.transform.localPosition.y - rect.rect.height;
+
+        bgStartPos = bottomBG.localPosition.y;
+        bgEndPos = bottomBG.localPosition.y - 40f;
 
         contentRect = pieceHolderTrm.parent.GetComponent<RectTransform>();
         invenHandler = GameManager.Instance.inventoryHandler;
 
-        invenBtn.onClick.AddListener(() => 
+        invenBtn.onClick.AddListener(() =>
         {
-            if(!isShow)
-                ShowInventoryInfo(); 
+            if (!isShow)
+                ShowInventoryInfo();
         });
         //usedInvenBtn.onClick.AddListener(() => { ShowInfoPanel(true); });
 
-        closeBtn.onClick.AddListener(() => 
+        closeBtn.onClick.AddListener(() =>
         {
             desPanel.ShowPanel(false);
             ShowInfoPanel(false);
         });
 
         ShowInfoPanel(false);
+        cg.alpha = 0f;
     }
 
     public void ShowInventoryInfo()
@@ -96,19 +109,36 @@ public class InventoryInfoHandler : MonoBehaviour
 
     private void ShowInfoPanel(bool enable)
     {
-        cg.alpha = enable ? 1f : 0f;
+        SetCGEnable(enable);
+        ShowCGEffect(enable);
+    }
+
+    private void SetCGEnable(bool enable)
+    {
+        cg.alpha = 1f;
         cg.blocksRaycasts = enable;
         cg.interactable = enable;
 
-        cg.transform.localPosition = startPos;
-
         isShow = enable;
+    }
 
+    private void ShowCGEffect(bool enable)
+    {
         if (enable)
         {
-            cg.transform.DOLocalMoveY(-rect.rect.height, 0.35f)
-                .From(true)
+            cg.transform.DOLocalMoveY(startPos, 0.35f)
                 .SetEase(Ease.OutBack, 0.7f);
+
+            bottomBG.transform.DOLocalMoveY(bgEndPos, 0.33f)
+                .SetEase(Ease.OutQuad);
+        }
+        else
+        {
+            cg.transform.DOLocalMoveY(endPos, 0.22f)
+                .SetEase(Ease.OutCubic);
+
+            bottomBG.transform.DOLocalMoveY(bgStartPos, 0.33f)
+                .SetEase(Ease.OutCubic);
         }
     }
 }
