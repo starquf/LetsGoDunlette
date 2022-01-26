58efc2a96a12ffaf5baf9e06b6eaa684120130a0
nexpang <rlarudgur123@gmail.com>
2022-01-26 14:27:55 +0900

    리워드 스킬 선택시 카드 설명 추가+ 잡버그 고침

--- Assets/Scripts/Handler/EncounterHandler.cs
+++ Assets/Scripts/Handler/EncounterHandler.cs
@@ -59,5 +59,6 @@ public class EncounterHandler : MonoBehaviour
     {
         print("인카운터 끝남");
         //GameManager.Instance.mapHandler.OpenMapPanel(true);
+        GameManager.Instance.battleHandler.StartBattle();
     }
 }
