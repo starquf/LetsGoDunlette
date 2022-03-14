a9655b4776882c4238eefb14f21fb5cd29f3fb77
승핵 <sping__@naver.com>
2022-03-14 21:42:28 +0900

    영구 양날의 검 C_Double_edged_sword 적에게 75의 피해를 입힌다. 50 - - 플레이어가 보호막이 없는 상태라면 플레이어가 50의 피해를 입는다. 제작

---
+++ Assets/Scripts/Piece/SkillPieces/Common/Skill_C_Double_edged_sword.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_C_Double_edged_sword : SkillPiece
+{
+    
+    public override void Cast(LivingEntity target, Action onCastEnd = null)
+    {
+
+        PlayerHealth playerHealth = owner.GetComponent<PlayerHealth>();
+        if (!playerHealth.HasShield())
+        {
+            playerHealth.GetDamage(50);
+        }
+        else
+        {
+            target.GetDamage(value);
+        }
+
+
+        Anim_C_SphereCast effect = PoolManager.GetItem<Anim_C_SphereCast>();
+        effect.transform.position = skillImg.transform.position;
+        effect.SetScale(0.5f);
+
+        effect.Play(() =>
+        {
+            onCastEnd?.Invoke();
+        });
+    }
+}
