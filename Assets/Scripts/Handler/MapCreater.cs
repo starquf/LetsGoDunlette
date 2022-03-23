67fcaee3dd6ca8122e394e6adfe408222846b28e
starquf <starquf321@gmail.com>
2022-03-23 20:29:17 +0900

    Awake Start로 수정

--- Assets/Scripts/Handler/MapCreater.cs
+++ Assets/Scripts/Handler/MapCreater.cs
@@ -44,7 +44,7 @@ public class MapCreater : MonoBehaviour
     public List<List<Node>> map = new List<List<Node>>();
     //private bool firstCreate = false;
 
-    private void Awake()
+    private void Start()
     {
         Init();
         CreateMap();
