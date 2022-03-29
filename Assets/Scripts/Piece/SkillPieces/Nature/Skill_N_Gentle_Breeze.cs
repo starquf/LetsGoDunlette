dfaab0a3ea37876f9e80b81190bbcebf301e9f72
승핵 <sping__@naver.com>
2022-03-29 14:29:54 +0900

    산들바람

---
+++ Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_Gentle_Breeze.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_N_Gentle_Breeze : SkillPiece
+{
+
+    public override void Cast(LivingEntity target, Action onCastEnd = null) //체력을 20 회복한 후 자연필드를 생성한다.
+    {
+        owner.GetComponent<PlayerHealth>().Heal(value);
+        GameManager.Instance.battleFieldHandler.SetFieldType(PatternType.Clover);
+
+        Anim_M_Recover effect = PoolManager.GetItem<Anim_M_Recover>();
+        effect.transform.position = GameManager.Instance.battleHandler.playerImgTrans.position;
+
+        effect.Play(() =>
+        {
+            onCastEnd?.Invoke();
+        });
+    }
+}
