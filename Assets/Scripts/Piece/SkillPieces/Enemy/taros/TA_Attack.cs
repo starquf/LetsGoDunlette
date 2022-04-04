7346a6bfcfd86da2f2efa7643f81b5058d6fabec
승핵 <sping__@naver.com>
2022-04-04 21:27:50 +0900

    타로스

---
+++ Assets/Scripts/Piece/SkillPieces/Enemy/taros/TA_Attack.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TA_Attack : SkillPiece
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        isPlayerSkill = false;
+    }
+
+    public override void Cast(LivingEntity target, Action onCastEnd = null)
+    {
+        SetIndicator(owner.gameObject, "АјАн").OnEnd(() =>
+        {
+            target.GetDamage(Value, this, owner);
+            GameManager.Instance.shakeHandler.ShakeBackCvsUI(2f, 0.2f);
+            Anim_M_Sword hitEffect = PoolManager.GetItem<Anim_M_Sword>();
+            hitEffect.transform.position = GameManager.Instance.enemyEffectTrm.position; hitEffect.SetScale(2);
+
+            hitEffect.Play(() =>
+            {
+                onCastEnd?.Invoke();
+            });
+        });
+    }
+}
