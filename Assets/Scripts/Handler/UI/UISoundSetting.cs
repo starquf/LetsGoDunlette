bf945fb79b0c2f92d0c0d16de9cca8d71817b331
승핵 <sping__@naver.com>
2022-04-14 15:09:33 +0900

    세팅설정 끝

---
+++ Assets/Scripts/Handler/UI/UISoundSetting.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISoundSetting : MonoBehaviour
+{
+    public Slider bgmSlider;
+    public Slider fxSlider;
+
+    public Text bgmText;
+    public Text fxText;
+
+    private List<Slider> sliders;
+
+    private void Start()
+    {
+        sliders = new List<Slider>
+        {
+            bgmSlider,
+            fxSlider
+        };
+
+        sliders.ForEach((x) =>
+        x.onValueChanged.AddListener((value) =>
+        {
+            x.value = value;
+            AdjustVolumes();
+        }));
+    }
+
+    private void AdjustVolumes()
+    {
+        SoundHandler.Instance.AdjustBGMVolume(bgmSlider.value);
+        bgmText.text = $"{(int)(bgmSlider.value * 100)}%";
+        SoundHandler.Instance.AdjustFxVoulme(fxSlider.value);
+        fxText.text = $"{(int)(fxSlider.value * 100)}%";
+    }
+}
