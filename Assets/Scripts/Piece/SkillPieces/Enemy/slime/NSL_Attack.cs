78046ecbb17e738a3b6344e2e05a82875b8078d0
승핵 <sping__@naver.com>
2022-05-02 18:02:25 +0900

    슬라임

--- Assets/Scripts/Piece/SkillPieces/Enemy/slime/NSL_Attack.cs
+++ Assets/Scripts/Piece/SkillPieces/Enemy/slime/NSL_Attack.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 public class NSL_Attack : SkillPiece
 {
@@ -8,11 +9,27 @@ public class NSL_Attack : SkillPiece
         isPlayerSkill = false;
     }
 
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+
+        desInfos[0].SetInfo(DesIconType.Attack, GetDamageCalc().ToString());
+
+        return desInfos;
+    }
+
+    private int GetDamageCalc()
+    {
+        int attack = (int)(owner.GetComponent<LivingEntity>().AttackPower * 0.4f); // 0.4 ¸¦ ¿¢¼¿¿¡¼­ °¡Á®¿Í¾ßÇÔ ¿¢¼¿
+
+        return attack;
+    }
+
     public override void Cast(LivingEntity target, Action onCastEnd = null)
     {
         SetIndicator(owner.gameObject, "°ø°Ý").OnEnd(() =>
         {
-            target.GetDamage(Value, this,owner);
+            target.GetDamage(GetDamageCalc(), this,owner);
             GameManager.Instance.shakeHandler.ShakeBackCvsUI(2f, 0.2f);
             Anim_M_Butt hitEffect = PoolManager.GetItem<Anim_M_Butt>();
             hitEffect.transform.position = GameManager.Instance.enemyEffectTrm.position; hitEffect.SetScale(2);
