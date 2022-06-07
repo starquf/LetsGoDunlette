c331dbe65dfe5b498ab3613bab7848a4ad9eb6d9
승핵 <sping__@naver.com>
2022-06-07 19:27:44 +0900

    타로스 끝

---
+++ Assets/Scripts/Enemy/Boss/Taros.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taros : MonoBehaviour
+{
+    private Action<SkillPiece, Action> skillEvent;
+    private SkillEvent skillEventInfo = null;
+
+    private Inventory owner;
+
+    public int patrolCount = 0;
+
+    public List<DesIconInfo> desInfos = new List<DesIconInfo>();
+    public PieceInfo pieceInfo;
+    public SkillPiece ta_Skill;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        owner = GetComponent<Inventory>();
+    }
+
+    public void TarosSkill()
+    {
+        GameManager.Instance.battleHandler.battleEvent.RemoveEventInfo(skillEventInfo);
+
+        skillEvent = (sp, action) =>
+        {
+            if (sp.Owner == owner) // 발동된 스킬이 타로스의 스킬이라면
+            {
+                action?.Invoke();
+                return;
+            }
+
+            if (owner.GetComponent<EnemyHealth>().IsDie || GameManager.Instance.battleHandler.player.IsDie)
+            {
+                action?.Invoke();
+                return;
+            }
+
+            if(sp.isTargeting == false)
+            {
+                action?.Invoke();
+                return;
+            }
+
+            if (patrolCount <= 0)
+            {
+                action?.Invoke();
+                return;
+            }
+
+            patrolCount--;
+            EnemyIndicator indi = owner.GetComponent<EnemyIndicator>();
+            indi.HideText();
+            desInfos[0].SetInfo(DesIconType.Attack, $"{GetDamageCalc(pieceInfo.GetValue())}");
+            desInfos[1].SetInfo(DesIconType.Shield, $"{pieceInfo.GetValue(1)}");
+            indi.ShowText("접근 금지", () =>
+            GameManager.Instance.battleHandler.castUIHandler.ShowCasting(pieceInfo, desInfos, ta_Skill, () =>
+            {
+                GameManager.Instance.GetPlayer().GetDamage(GetDamageCalc(pieceInfo.GetValue()), owner);
+                owner.GetComponent<EnemyHealth>().AddShield(pieceInfo.GetValue(1));
+                GameManager.Instance.animHandler.GetAnim(AnimName.M_Sword)
+                .SetPosition(GameManager.Instance.enemyEffectTrm.position)
+                .SetScale(2)
+                .Play(() =>
+                {
+                    GameManager.Instance.battleHandler.battleUtil.SetTimer(0.5f + 0.25f, () =>
+                    {
+                        GameManager.Instance.battleHandler.castUIHandler.ShowPanel(false, false);
+                        action?.Invoke();
+                    });
+                });
+            }));
+        };
+
+        skillEventInfo = new SkillEvent(EventTimeSkill.AfterSkill, skillEvent);
+        GameManager.Instance.battleHandler.battleEvent.BookEvent(skillEventInfo);
+    }
+
+    public int GetDamageCalc(int addValue) //Player 전용임
+    {
+        int attack = owner.GetComponent<LivingEntity>().AttackPower + addValue;
+        return attack;
+    }
+}
