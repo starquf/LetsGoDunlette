621adfe553439c98f4b3b00d7ef7904e563f95aa
승핵 <sping__@naver.com>
2022-06-16 16:40:20 +0900

    고전압 구현

---
+++ Assets/Scripts/Piece/SkillPieces/Eletric/Skill_E_High_Voltage.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Skill_E_High_Voltage : SkillPiece
+{
+    private Action<Action> highVolatge = null;
+    private NormalEvent normalEvent = null;
+
+    protected override void Start()
+    {
+        highVolatge = (a) => {
+            if(Owner.GetComponent<LivingEntity>().GetShieldHp() <= 0)
+            {
+                for (int i = 0; i < bh.enemys.Count; i++)
+                {
+                    bh.enemys[i].cc.SetCC(CCType.Stun,1);
+                }
+            }
+            a?.Invoke();
+        };
+
+        normalEvent = new NormalEvent(true, 1, highVolatge, EventTime.EndOfTurn);
+    }
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+        desInfos[0].SetInfo(DesIconType.Shield, $"{Value}");
+        return desInfos;
+    }
+
+    public override void Cast(LivingEntity target, Action onCastEnd = null)
+    {
+        SetIndicator(Owner.gameObject, "°íÀü¾Ð").OnEndAction(() =>
+        {
+            Owner.GetComponent<LivingEntity>().AddShield(Value);
+
+            bh.battleEvent.BookEvent(normalEvent);
+
+            animHandler.GetAnim(AnimName.SkillEffect01)
+            .SetPosition(Owner.transform.position)
+            .SetScale(2f)
+            .SetRotation(Vector3.forward * -90f)
+            .Play(() =>
+            {
+                onCastEnd?.Invoke();
+            });
+        });
+    }
+}
