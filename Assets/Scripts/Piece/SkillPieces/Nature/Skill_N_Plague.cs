d977691fed4c6ade2660df071c71e8b08f10ec58
승핵 <sping__@naver.com>
2022-06-16 15:40:44 +0900

    역병구현

---
+++ Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_Plague.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_N_Plague : SkillPiece
+{
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+        desInfos[0].SetInfo(DesIconType.Wound, $"{Value}");
+        return desInfos;
+    }
+
+
+    public override void Cast(LivingEntity target, Action onCastEnd = null) //대상을 제외한 모든 적에게 대상의 <sprite=12>와 같은 수치의 <sprite=12>를 부여한다.
+    {
+        SetIndicator(Owner.gameObject, "역병").OnEndAction(() =>
+        {
+            target.cc.SetCC(CCType.Wound, Value);
+            List<EnemyHealth> enemys = bh.enemys;
+            for (int i = 0; i < enemys.Count; i++)
+            {
+                if (enemys[i] != target)
+                {
+                    enemys[i].cc.SetCC(CCType.Wound, target.cc.GetCCValue(CCType.Wound));
+                }    
+            }
+
+            animHandler.GetAnim(AnimName.SkillEffect01)
+            .SetPosition(Owner.transform.position)
+            .SetScale(2f)
+            .SetRotation(Vector3.forward * -90f)
+            .Play(() =>
+            {
+                onCastEnd?.Invoke();
+            });
+        });
+    }
+}
