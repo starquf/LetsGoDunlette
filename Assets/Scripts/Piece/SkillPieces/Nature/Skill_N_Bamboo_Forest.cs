a363d2f6b96b4939a1ed475b14ffc5f6389422a4
승핵 <sping__@naver.com>
2022-06-16 15:40:25 +0900

    대나무숲 구현

---
+++ Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_Bamboo_Forest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_N_Bamboo_Forest : SkillPiece
+{
+    private GameObject bambooSpear;
+
+    protected override void Start()
+    {
+        base.Start();
+        bambooSpear = GameManager.Instance.skillContainer.GetSkillPrefab<Skill_N_Bamboo_Spear>();
+    }
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+        return desInfos;
+    }
+
+
+    public override void Cast(LivingEntity target, Action onCastEnd = null) //자연 개수 + 개
+    {
+        SetIndicator(Owner.gameObject, "조각 추가").OnEndAction(() =>
+        {
+            animHandler.GetAnim(AnimName.SkillEffect01)
+            .SetPosition(Owner.transform.position)
+            .SetScale(2f)
+            .SetRotation(Vector3.forward * -90f)
+            .Play(() =>
+            {
+                List<RulletPiece> rulletPieces = bh.mainRullet.GetPieces();
+                for (int i = 0; i < rulletPieces.Count; i++)
+                {
+                    if (rulletPieces[i] != null)
+                    {
+                        if (rulletPieces[i].currentType == ElementalType.Nature)
+                        {
+                            bh.battleUtil.SetTimer(0.25f * i, () => { GameManager.Instance.inventoryHandler.CreateSkill(bambooSpear, Owner, Owner.transform.position); });
+                        }
+                    }
+                }
+
+                bh.battleUtil.SetTimer(0.5f, onCastEnd);
+            });
+        });
+    }
+}
