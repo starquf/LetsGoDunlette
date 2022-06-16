5dad6d13ceefbf2bccc4a01572e13554ea772369
승핵 <sping__@naver.com>
2022-06-16 14:09:47 +0900

    방패던지기 구현

---
+++ Assets/Scripts/Piece/SkillPieces/Common/Skill_C_Shield_Throwing.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public class Skill_C_Shield_Throwing : SkillPiece
+{
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+        return desInfos;
+    }
+
+    public override void Cast(LivingEntity target, Action onCastEnd = null)
+    {
+        LivingEntity health = Owner.GetComponent<LivingEntity>();
+        if (health.HasShield())
+        {
+            target.GetDamage(health.GetShieldHp());
+        }
+        else
+        {
+            target.RemoveShield();
+        }
+
+        animHandler.GetAnim(AnimName.E_ManaSphereHit)
+            .SetScale(0.5f)
+            .SetPosition(skillIconImg.transform.position)
+            .Play(() =>
+            {
+                onCastEnd?.Invoke();
+            });
+    }
+}
