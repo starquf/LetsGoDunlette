1f9d7dbe16566cf87922cc0cd5e959386306d1b2
승핵 <sping__@naver.com>
2022-06-21 09:49:05 +0900

    부메랑 기능 버그 수정

--- Assets/Scripts/Piece/SkillPieces/Common/Skill_C_Boomerang.cs
+++ Assets/Scripts/Piece/SkillPieces/Common/Skill_C_Boomerang.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 public class Skill_C_Boomerang : SkillPiece
 {
     private int originValue = 0;
+    private EventInfo eventInfo;
 
     protected override void Start()
     {
         base.Start();
         originValue = value;
+
+        GameManager.Instance.battleHandler.battleEvent.RemoveEventInfo(eventInfo);
+
+
+        eventInfo = new NormalEvent(new Action<Action>(ResetValue), EventTime.EndBattle);
+        GameManager.Instance.battleHandler.battleEvent.BookEvent(eventInfo);
     }
+
     public override List<DesIconInfo> GetDesIconInfo()
     {
         base.GetDesIconInfo();
@@ -17,16 +25,17 @@ public class Skill_C_Boomerang : SkillPiece
         return desInfos;
     }
 
-    public override void ResetPiece()
-    {
-        base.ResetPiece();
-        value = originValue;
-    }
-
     public override void Cast(LivingEntity target, Action onCastEnd = null)
     {
         target.GetDamage(GetDamageCalc());
         value++;
+
+        animHandler.GetTextAnim()
+               .SetType(TextUpAnimType.Up)
+               .SetPosition(skillIconImg.transform.position)
+               .SetScale(0.8f)
+               .Play("강화!");
+
         animHandler.GetAnim(AnimName.E_ManaSphereHit)
             .SetScale(0.5f)
             .SetPosition(skillIconImg.transform.position)
@@ -34,5 +43,11 @@ public class Skill_C_Boomerang : SkillPiece
             {
                 onCastEnd?.Invoke();
             });
+    }
+
+    private void ResetValue(Action action) //전투가 끝나면 피해가 초기화
+    {
+        value = originValue;
+        action?.Invoke();
     }
 }
