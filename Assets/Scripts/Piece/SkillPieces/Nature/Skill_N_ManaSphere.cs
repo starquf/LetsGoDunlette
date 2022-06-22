95189517463066b1b67c0eb8db2517e3bd7f79a9
승핵 <sping__@naver.com>
2022-06-22 18:45:15 +0900

    이펙트 시작위치 변경

--- Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_ManaSphere.cs
+++ Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_ManaSphere.cs
@@ -27,7 +27,7 @@ public class Skill_N_ManaSphere : SkillPiece
         //print($"스킬 발동!! 이름 : {PieceName}");
 
         Vector3 targetPos = target.transform.position;
-        Vector3 startPos = transform.position;
+        Vector3 startPos = Owner.transform.position;
 
         EffectObj skillEffect = PoolManager.GetItem<EffectObj>();
         skillEffect.transform.position = startPos;
