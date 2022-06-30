202fd46ac78436c147b7b83b969d9a21000215c6
nexpang <rlarudgur123@gmail.com>
2022-06-30 16:15:59 +0900

    화상, 역병 이펙트 수정

--- Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_Plague.cs
+++ Assets/Scripts/Piece/SkillPieces/Nature/Skill_N_Plague.cs
@@ -18,24 +18,37 @@ public class Skill_N_Plague : SkillPiece
 
     public override void Cast(LivingEntity target, Action onCastEnd = null) //대상을 제외한 모든 적에게 대상의 <sprite=12>와 같은 수치의 <sprite=12>를 부여한다.
     {
-
-        target.cc.SetCC(CCType.Wound, Value);
-        List<EnemyHealth> enemys = bh.enemys;
-        for (int i = 0; i < enemys.Count; i++)
-        {
-            if (enemys[i] != target)
-            {
-                enemys[i].cc.SetCC(CCType.Wound, target.cc.GetCCValue(CCType.Wound));
-            }
-        }
-
-        animHandler.GetAnim(AnimName.SkillEffect01)
-        .SetPosition(Owner.transform.position)
-        .SetScale(2f)
-        .SetRotation(Vector3.forward * -90f)
+        animHandler.GetAnim(AnimName.N_PoisionCloud)
+        .SetPosition(target.transform.position)
+        .SetScale(1f)
         .Play(() =>
         {
-            onCastEnd?.Invoke();
+            List<EnemyHealth> enemys = bh.enemys;
+            if(enemys.Count !=1)
+            {
+                for (int i = 0; i < enemys.Count; i++)
+                {
+                    int idx = i;
+                    if (enemys[i] != target)
+                    {
+                        animHandler.GetAnim(AnimName.N_PoisionCloud)
+                        .SetPosition(target.transform.position)
+                        .SetScale(1f)
+                        .Play();
+                        enemys[i].cc.SetCC(CCType.Wound, target.cc.GetCCValue(CCType.Wound));
+                        if (i == enemys.Count - 1)
+                        {
+                            onCastEnd?.Invoke();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                onCastEnd?.Invoke();
+            }
         });
+        target.cc.SetCC(CCType.Wound, Value);
+
     }
 }
