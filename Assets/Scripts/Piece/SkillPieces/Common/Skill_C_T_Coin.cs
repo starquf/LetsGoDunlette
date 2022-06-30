452a9c6d16f2a1334327b2fb79da08b3d1b5cb5f
nexpang <rlarudgur123@gmail.com>
2022-06-30 16:41:17 +0900

    트릭:동전,트릭: 비둘기 이펙트 수정

--- Assets/Scripts/Piece/SkillPieces/Common/Skill_C_T_Coin.cs
+++ Assets/Scripts/Piece/SkillPieces/Common/Skill_C_T_Coin.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Skill_C_T_Coin : SkillPiece
 {
+    public Sprite coinSpr;
+    private Gradient effectGradient;
+
     protected override void Start()
     {
         base.Start();
+        effectGradient = GameManager.Instance.inventoryHandler.effectGradDic[ElementalType.None];
     }
 
     public override List<DesIconInfo> GetDesIconInfo()
@@ -16,7 +22,60 @@ public class Skill_C_T_Coin : SkillPiece
 
     public override void Cast(LivingEntity target, Action onCastEnd = null) //"50 골드를 얻는다. 전투 종료 시 또는 사용 후 삭제된다."
     {
-        GameManager.Instance.AddGold(value);
-        onCastEnd?.Invoke();
+        Vector3 targetPos = target.transform.position;
+        Vector3 startPos = Owner.transform.position;
+
+
+        EffectObj skillEffect = PoolManager.GetItem<EffectObj>();
+        skillEffect.transform.position = startPos;
+        skillEffect.SetSprite(coinSpr);
+        skillEffect.SetColorGradient(effectGradient);
+        skillEffect.SetScale(Vector3.one * 0.3f);
+
+
+        skillEffect.Play(targetPos, () =>
+        {
+            animHandler.GetAnim(AnimName.C_ManaSphereHit)
+            .SetPosition(targetPos)
+            .SetScale(0.5f)
+            .Play();
+
+            target.GetDamage(1, currentType);
+
+            GameManager.Instance.cameraHandler.ShakeCamera(0.5f, 0.15f);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int a = i;
+
+                startPos.x = 0;
+
+                EffectObj skillEffect1 = PoolManager.GetItem<EffectObj>();
+                skillEffect1.transform.position = targetPos;
+                skillEffect1.SetSprite(coinSpr);
+                skillEffect1.SetColorGradient(effectGradient);
+                skillEffect1.SetScale(Vector3.one * 0.3f);
+
+                skillEffect1.Play(startPos, () =>
+                {
+                    animHandler.GetAnim(AnimName.C_ManaSphereHit)
+                    .SetPosition(targetPos)
+                    .SetScale(0.5f)
+                    .Play();
+
+                    GameManager.Instance.cameraHandler.ShakeCamera(0.5f, 0.15f);
+
+                    if (a >= 9)
+                    {
+                        GameManager.Instance.AddGold(value);
+                        onCastEnd?.Invoke();
+                    }
+
+                    skillEffect1.EndEffect();
+                }, BezierType.Cubic, isRotate: true, playSpeed: 2f, delay: i * 0.05f);
+            }
+
+            skillEffect.EndEffect();
+        }, BezierType.Linear, isRotate: true, playSpeed: 2f);
     }
 }
