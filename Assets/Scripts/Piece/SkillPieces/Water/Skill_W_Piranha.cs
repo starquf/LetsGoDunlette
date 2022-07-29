062beb60c12f696a7df79e902763f26103acf751
승핵 <sping__@naver.com>
2022-07-29 14:37:16 +0900

    피라냐

---
+++ Assets/Scripts/Piece/SkillPieces/Water/Skill_W_Piranha.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Skill_W_Piranha : SkillPiece
+{
+    public override void Cast(LivingEntity target, Action onCastEnd = null) //대상에게 <sprite=12>가 있을 시 <sprite=12>5 부여
+    {
+        if (target.cc.IsCC(CCType.Wound))
+        {
+            target.cc.IncreaseCCTurn(CCType.Wound, 5);
+        }
+
+        target.GetDamage(GetDamageCalc(value));
+    }
+
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+        desInfos[0].SetInfo(DesIconType.Attack, $"{Value}");
+        return desInfos;
+    }
+}
