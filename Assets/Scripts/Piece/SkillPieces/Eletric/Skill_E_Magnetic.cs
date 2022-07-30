bef12bd13a178c0197363d93c83fb999b12ae71f
승핵 <sping__@naver.com>
2022-07-30 10:13:18 +0900

    나머지 스킬들 틀 만들어두고

---
+++ Assets/Scripts/Piece/SkillPieces/Eletric/Skill_E_Magnetic.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Skill_E_Magnetic : SkillPiece
+{
+    public override void Cast(LivingEntity target, Action onCastEnd = null) 
+    {
+        target.GetDamage(GetDamageCalc(value));
+    }
+
+    public override List<DesIconInfo> GetDesIconInfo()
+    {
+        base.GetDesIconInfo();
+        desInfos[0].SetInfo(DesIconType.Attack, $"{Value}");
+        return desInfos;
+    }
+}
